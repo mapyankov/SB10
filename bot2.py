70bd18065f9c07196c7e8db55c5bd544773b8a36
mapyankov <126340359+mapyankov@users.noreply.github.com>
2024-08-31 11:18:20 +0300

    авмв
    авм

---
+++ bot2.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run("MTI3NjgwOTI5NjU5OTk3Mzk4Mw.GRsc_q.pBM8ShCNFhAvI9YDsl-rrkKjbJipq7bXGIuW4U")
